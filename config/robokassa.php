c23cab6f9aeebf4007dcb0c880d6c2152ccf462d
user <user@MacBook-Air-user.local>
2016-02-01 13:38:13 +0400

    update robokassa

--- config/robokassa.php
+++ config/robokassa.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'login'      => 'itemupru',
-    'password1'  => '5B7oYePZLj8xgao',
-    'password2'  => '8MhxNbHNwVB493V',
+    'login'      => 'itemup',
+    'password1'  => 'PBcnB1JHTy8Tp69g4xHI', //HuMqzw6RmQ2AHWG594Nj
+    'password2'  => 'V3UKAkw19xr3VPUOme5N', //urS8vbAi2WkjE97xDe2Y
     'testmode'   => false,
     'url'		 => 'http://itemup.ru'
 ];
