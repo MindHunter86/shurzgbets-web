7c8347a8691ee4955c3eb2e46f04db399c27f6a3
oldsergo <oldsergo.pro@gmail.com>
2016-02-15 16:23:25 +0400

    lottery

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -279,10 +279,10 @@ class GameController extends Controller
         if(is_null($newBet)) {
             return false;
         }
-        $bets[] = $newBet;
+        //$bets[] = $newBet;
         $create = Lottery::create([
             'rand_number' => $rand_number,
-            'items' => json_encode($bets),
+            'items' => json_encode($newBet),
             'price' => $newBet['price'],
             'max' => round($newBet['price'] * 3)
         ]);
@@ -292,8 +292,8 @@ class GameController extends Controller
         ];
         $newBet->delete();
 
-        $this->redis->set('current.lottery', $lottery['id']);
-        return response()->json($lottery);
+        //$this->redis->set('current.lottery', $lottery['id']);
+        //return response()->json($lottery);
     }
 
     public function checkOffer()
