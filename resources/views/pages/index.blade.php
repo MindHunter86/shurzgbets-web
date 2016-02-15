75b5e8f5107ed783c81aa852a7e9169e73837ede
oldsergo <oldsergo.pro@gmail.com>
2016-02-15 18:06:07 +0400

    giveaway

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 
 @section('content')
+    <!--
     <div class="none">
         <div class="box-modal" id="modal-6" style="width:450px;">
             <div class="box-modal-top"><div class="box-modal_close arcticmodal-close"></div>Список участников раздачи</div>
@@ -29,7 +30,7 @@
         </div>
         <div class="hoax-item">
             <div class="hoax-item-images"><div class="hoax-item-img"><b class="ellipsis">{{ $lottery->items->market_hash_name }}</b> Стоимость: <b>{{ $lottery->price }}</b> руб.</div><img src="https://steamcommunity-a.akamaihd.net/economy/image/class/{{ \App\Http\Controllers\GameController::APPID }}/{{ $lottery->items->classid }}/200fx200f" alt="" /></div>
-            <div class="hoax-item-time">00:00:00</div>
+            <div class="hoax-item-time">{{ $lottery->price }} руб.-</div>
         </div>
         <div class="hoax-right">
             <div class="hoax-button"></div>
@@ -38,7 +39,8 @@
                 <a href="#" onclick="$('#modal-1').arcticmodal(); return false;">Как это работает?</a>
             </div>
         </div>
-    </div>
+    </div>-->
+
     <div class="information full">
         <div class="info-in">
             <div class="info-images"><img src="{{ asset('new/images/info1.png') }}" alt="" /></div>
