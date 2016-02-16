acbcbf9b89908bcd2384e64d60077291bdebeebd
oldsergo <oldsergo.pro@gmail.com>
2016-02-16 20:58:12 +0400

    giveaway

--- resources/views/includes/lottery.blade.php
+++ resources/views/includes/lottery.blade.php
@@ -1,1 +1,1 @@
-<li><img data-profile="{{ $bet->user->steamid64 }}" src="{{ $player->user->avatar }}"/></li>
+<li><img data-profile="{{ $player->user->steamid64 }}" src="{{ $player->user->avatar }}"/></li>
