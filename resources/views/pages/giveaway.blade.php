f11f998c8d3722cffdcedb41c7feef78fe896efb
oldsergo <oldsergo.pro@gmail.com>
2016-02-17 11:28:34 +0400

    giveaway

--- resources/views/pages/giveaway.blade.php
+++ resources/views/pages/giveaway.blade.php
@@ -9,13 +9,12 @@
             <div class="inv_table_panel">
                 <div class="type1">Предмет</div>
                 <div class="type2">Победитель</div>
-                <div class="type3">Участников</div>
+                <div class="type3">Участники</div>
             </div>
         </div>
         @foreach($lottery as $lot)
             <div class="inv_table_info">
 
-                <div class="type2"><a href="#" data-profile="{{ $lot->winner->steamid64 }}">{{ $lot->winner->username }}</a></div>
                 <div class="type3">
                     <span style="color:green;">{{ $lot->players }} / {{ $lot->max }}</span>
                 </div>
