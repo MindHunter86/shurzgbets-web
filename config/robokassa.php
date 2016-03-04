e3eb8a9a4ce588950ec433d49be7b5f49bba91c5
Sergo <sergo@mh00.net>
2016-03-04 23:52:52 +0300

    'to'

--- config/robokassa.php
+++ config/robokassa.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'login'      => '24436',
-    'password1'  => 'zc7kfbt9', //PBcnB1JHTy8Tp69g4xHI
-    'password2'  => 'w4yj5rk9', //V3UKAkw19xr3VPUOme5N
+    'login'      => 'itemup',
+    'password1'  => 'PBcnB1JHTy8Tp69g4xHI', //PBcnB1JHTy8Tp69g4xHI
+    'password2'  => 'V3UKAkw19xr3VPUOme5N', //V3UKAkw19xr3VPUOme5N
     'testmode'   => false,
     'url'		 => 'http://itemup.ru'
 ];
