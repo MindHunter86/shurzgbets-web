720e7f82087a32c983cb1f767bcfb2366fdaeb98
Sergo <sergo@mh00.net>
2016-03-19 12:11:11 +0300

    '1'

--- public/assets/js/newapp.js
+++ public/assets/js/newapp.js
@@ -421,7 +421,7 @@ if (START) {
                     $('.winner-name span').html('<a data-profile="' + data.winner.steamid64 + '" href="#"></a>');
                     $('.winner-name span a').text(replaceLogin(data.winner.username));
                     $('.winner-name u').text('(' + data.chance + '%)');
-                }, 1200 * timeout);
+                }, 1050 * timeout);
             }
         })
         .on('newGame', function (data) {
