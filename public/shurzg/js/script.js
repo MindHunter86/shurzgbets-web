56b88e955c8e85acacfcfbcc342ddfed2b4537a0
Sergo <sergo@mh00.net>
2016-03-23 23:31:44 +0300

    init

---
+++ public/shurzg/js/script.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function(){
+	var cookie = $.cookie('language');
+	if (cookie == 'active') {
+		$('.language_ico').addClass('active');
+	}
+});
+
+function language() {
+	var lang = $('.language_ico').hasClass('active');
+	if(lang) {
+		$('.language_ico').removeClass('active');
+		$.cookie('language', null);
+	} else {
+		$('.language_ico').addClass('active');
+		$.cookie('language', 'active');
+	}
+}
+
+function chat() {
+	var chat = $('.chat').hasClass('active');
+	if(chat) {
+		$('.chat').removeClass('active').animate({right: '-334px'}, 300);
+	} else {
+		$('.chat').addClass('active').animate({right: '0'}, 600);
+	}
+}
