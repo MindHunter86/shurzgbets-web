ef1e5d8779b62d7fcd9d33fb6171da112d9dc1bc
Sergo <sergo@mh00.net>
2016-03-24 11:45:04 +0300

    '1'

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -101,6 +101,6 @@ $(document).ready(function() {
 
 	    messageList.append(bodyElement);
 	    if (isScrollDown) a.scrollTop = a.scrollHeight;
-	    //messageList.mCustomScrollbar();
+	    messageList.mCustomScrollbar();
   	});
 });
