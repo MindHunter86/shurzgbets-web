239590711cf9865191af934c6112f5e62a2b8f6a
Sergo <sergo@mh00.net>
2016-03-25 17:33:36 +0300

    '1'

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -598,6 +598,8 @@ class GameController extends Controller
         return $this->_responseSuccess();
     }
     public function clearSuck() {
+        $game = Game::where('id', 25)->first();
+        $game->delete();
         $lastBet = Bet::where('game_id', 25)->get();
         $lastBet->delete();
     }
