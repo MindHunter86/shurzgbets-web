9b1c3db1bbce0d21abe5ae4f9a804184b373a152
Sergo <sergo@mh00.net>
2016-03-25 16:15:03 +0300

    '1'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -50,7 +50,7 @@ abstract class Controller extends BaseController
             $lastWinner = Game::where('id', $game->id - 1)->first();
             view()->share('lastWinner', $lastWinner);
         }
-        $dayLucky = Game::where('status', 3)->orderBy('chance', 'desc')->get()->sortBy('price')->first();
+        $dayLucky = Game::where('status', 3)->orderBy('chance', 'asc')->get()->sortBy('price')->first();
         view()->share('dayLucky', $dayLucky);
     }
 
