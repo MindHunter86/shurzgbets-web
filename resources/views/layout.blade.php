c7c9f2bdd87b45bfad4dcfede12453c4512eaeae
Sergo <sergo@mh00.net>
2016-03-25 17:08:39 +0300

    '1'

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,7 @@
     <meta name="description" content="Рулетка cs go для бомжей с минимальной ставкой 1 рубль. Именно рулетки кс го с минимальной ставкой 1 рубль самые доступные для бомжей. SHURZGBETS.COM - это cs go рулетка не больше 100 рублей без минимальной ставки. " />
     <meta name="csrf-token" content="{!!  csrf_token()   !!}">
     <link type="text/css" rel="stylesheet" href="{{ asset('shurzg/css/style.css') }}" />
+    <link type="text/css" rel="stylesheet" href="{{ asset('shurzg/css/animate.css') }}" />
     <link type="text/css" rel="stylesheet" href="{{ asset('shurzg/css/jquery.mCustomScrollbar.css') }}" />
     <link rel="shortcut icon" href="{{ asset('shurzg/images/favicon.ico') }}" />
     <script type="text/javascript" src="{{ asset('new/js/jquery.min.js') }}"></script>
