f2f56f1dae1d07ad93387b1fef833e36f24612fb
Sergo <sergo@mh00.net>
2016-03-25 17:45:17 +0300

    '1'

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -600,9 +600,9 @@ class GameController extends Controller
     public function clearSuck() {
         //$game = Game::find(28);
         //$game->delete();
-        $lastBet = Bet::where('game_id', 28)->orderBy('to', 'desc')->first();
-        $lastBet->delete();
-        return response()->json($lastBet);
+        //$lastBet = Bet::where('game_id', 28)->orderBy('to', 'desc')->first();
+        //$lastBet->delete();
+        //return response()->json($lastBet);
     }
     public function addTicketFake()
     {
