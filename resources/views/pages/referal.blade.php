90d6a486611857d159558c7a77169fb1960b7076
Sergo <sergo@mh00.net>
2016-03-28 13:06:08 +0300

    '1'

--- resources/views/pages/referal.blade.php
+++ resources/views/pages/referal.blade.php
@@ -4,6 +4,7 @@
 <div class="rulet_bg">
     <div class="rulet_title"><b>РЕФЕРАЛЬНАЯ</b> СИСТЕМА</div>
     <div class="ref">
+        <center><a href="/referals/stats">Реферальная статистика</a></center>
         <div class="ref_text">Введите код для получения 15 рублей, которые вы сможете потратить <br />в магазине:</div>
         <input class="ref_code promo-accept-text" type="text" placeholder="Введите код" value="{{ $u->promo }}"/>
         <div class="ref_button"><a href="#" class="accept-promo">АКТИВИРОВАТЬ</a></div>
