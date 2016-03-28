0984e7743441b6e13b2195f679cbffcf0fc59dcd
Sergo <sergo@mh00.net>
2016-03-28 15:37:05 +0300

    '1'

--- resources/views/pages/giveaway.blade.php
+++ resources/views/pages/giveaway.blade.php
@@ -1,27 +1,26 @@
+
+
 @extends('layout')
 
 @section('content')
-<div class="content_bg">
-    <div class="full">
-        <div class="content_title"><div>История <b>раздач</b></div></div>
-        <div class="clear"></div>
-        <div class="inv_table">
-            <div class="inv_table_panel">
-                <div class="type1">Предмет</div>
-                <div class="type2">Победитель</div>
-                <div class="type3">Участники</div>
-            </div>
+<div class="rulet_bg">
+    <div class="rulet_title"><b>История </b>раздач</div>
+    <div class="table">
+        <div class="table_panel">
+            <div class="t1"></div>
+            <div class="t2">Предмет</div>
+            <div class="t3">Победитель</div>
+            <div class="t4">Участники</div>
         </div>
         @foreach($lottery as $lot)
-            <div class="inv_table_info">
-                <div class="type1">
-                    <div><img src="https://steamcommunity-a.akamaihd.net/economy/image/class/730/{{$lot->items->classid}}/200fx200f"/></div>
-                    {{$lot->items->market_hash_name}}
+            <div class="table_info">
+                <div class="t1"></div>
+                <div class="t2">
+                    <em class="tava"><img src="https://steamcommunity-a.akamaihd.net/economy/image/class/730/{{$lot->items->classid}}/200fx200f" alt="" /></em>
+                    <em class="tname ell">{{$lot->items->market_hash_name}}</em>
                 </div>
-                <div class="type2"><a href="#" data-profile="{{ $lot->winner->steamid64 }}">{{ $lot->winner->username }}</a></div>
-                <div class="type3">
-                    <span style="color:green;">{{ $lot->players }} / {{ $lot->max }}</span>
-                </div>
+                <div class="t3">{{ $lot->winner->username }}</div>
+                <div class="t4">{{ $lot->players }} / {{ $lot->max }}</div>
             </div>
         @endforeach
     </div>
