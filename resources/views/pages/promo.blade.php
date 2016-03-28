9220223eabd56b5cac7836b1d4caaf956640b1dd
Sergo <sergo@mh00.net>
2016-03-28 13:03:23 +0300

    '1'

--- resources/views/pages/promo.blade.php
+++ resources/views/pages/promo.blade.php
@@ -1,24 +1,22 @@
+
 @extends('layout')
 
 @section('content')
-<div class="content_bg">
-    <div class="full">
-        <div class="content_title"><div>Ваши <b>ПАРТНЕРСКИЕ НАЧИСЛЕНИЯ</b></div></div>
-        <div class="clear"></div>
-        <div class="inv_table">
-            <div class="inv_table_panel">
-                <div class="type1">Реферал</div>
-                <div class="type2">Ставка</div>
-                <div class="type3">Ваш доход</div>
-            </div>
+<div class="rulet_bg">
+    <div class="rulet_title"><b>Реферальная </b>статистика</div>
+    <div class="table">
+        <div class="table_panel">
+            <div class="t1">Реферал</div>
+            <div class="t2">Ставка</div>
+            <div class="t3">Ваш доход</div>
         </div>
         @foreach($referal as $refa)
             @foreach($refa as $key => $ref)
-                <div class="inv_table_info">
-                    <div class="type1"><a href="#" data-profile="{{ $ref->user->steamid64 }}">{{ $ref->user->username }}</a></div>
-                    <div class="type2">{{ $ref->price }} руб.</div>
-                    <div class="type3">{{ round(($ref->price / 100) * 1)  }} руб.</div>
-                </div>
+            <div class="table_info">
+                <div class="t1">{{ $ref->user->username }}</div>
+                <div class="t2">{{ $ref->price }} руб.</div>
+                <div class="t3">{{ round(($ref->price / 100) * 1)  }} руб.</div>
+            </div>
             @endforeach
         @endforeach
     </div>
