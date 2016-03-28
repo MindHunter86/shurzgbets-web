2c74ce4393fb9147ef79e0c31b307f668bb01866
Sergo <sergo@mh00.net>
2016-03-28 13:05:09 +0300

    '1'

--- resources/views/pages/promo.blade.php
+++ resources/views/pages/promo.blade.php
@@ -6,16 +6,17 @@
     <div class="rulet_title"><b>Реферальная </b>статистика</div>
     <div class="table">
         <div class="table_panel">
-            <div class="t1">Реферал</div>
-            <div class="t2">Ставка</div>
-            <div class="t3">Ваш доход</div>
+            <div class="t1"></div>
+            <div class="t2">Реферал</div>
+            <div class="t3">Ставка</div>
+            <div class="t4">Ваш доход</div>
         </div>
         @foreach($referal as $refa)
             @foreach($refa as $key => $ref)
             <div class="table_info">
-                <div class="t1">{{ $ref->user->username }}</div>
-                <div class="t2">{{ $ref->price }} руб.</div>
-                <div class="t3">{{ round(($ref->price / 100) * 1)  }} руб.</div>
+                <div class="t2">{{ $ref->user->username }}</div>
+                <div class="t3">{{ $ref->price }} руб.</div>
+                <div class="t4">{{ round(($ref->price / 100) * 1)  }} руб.</div>
             </div>
             @endforeach
         @endforeach
