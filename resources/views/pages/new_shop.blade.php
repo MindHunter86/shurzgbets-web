f0620f75d22444ec206b1b3860515cb9d0df3849
Sergo <sergo@mh00.net>
2016-03-28 16:00:32 +0300

    '1'

--- resources/views/pages/new_shop.blade.php
+++ resources/views/pages/new_shop.blade.php
@@ -1,6 +1,65 @@
 @extends('layout')
 
 @section('content')
+<div class="rulet_bg" style="overflow:visible;">
+    <div class="rulet_title"><b>Магазин</b> предметов</div>
+    <div class="shop_panel">
+    </div>
+    <div class="shop_select">
+        <em>Категории:</em>
+        <select class="intro-select1" id="searchType" multiple="multiple">
+            <option value="Knife">Knife</option>
+            <option value="Rifle">Rifle</option>
+            <option value="Shotgun">Shotgun</option>
+            <option value="Sniper Rifle">Sniper Rifle</option>
+            <option value="Pistol">Pistol</option>
+            <option value="SMG">SMG</option>
+            <option value="Machinegun">Machinegun</option>
+            <option value="Container">Container</option>
+            <option value="Sticker">Sticker</option>
+            <option value="Music Kit">Music Kit</option>
+            <option value="Key">Key</option>
+            <option value="Pass">Pass</option>
+            <option value="Gift">Gift</option>
+            <option value="Tag">Tag</option>
+            <option value="Tool">Tool</option>
+        </select>
+    </div>
+    <div class="shop_select shop_select2">
+        <em>Качество:</em>
+        <select class="intro-select1" id="searchQuality" multiple="multiple">
+            <select id="searchQuality" multiple="multiple">
+            <option value="Factory new">Factory new</option>
+            <option value="Minimal Wear">Minimal Wear</option>
+            <option value="Field-Tested">Field-Tested</option>
+            <option value="Well-Worn">Well-Worn</option>
+            <option value="Battle-Scarred">Battle-Scarred</option>
+            <option value="Normal">Normal</option>
+        </select>
+    </div>
+    <input type="text" id="searchName" name="searchName" placeholder="Поиск" class="shop_search" />
+    <div class="clear"></div>
+    <div class="shop_line">
+        <ul class="shop_sort">
+            <em>Сортировать по:</em>
+            <li class="active">Цене</li>
+            <li>Названию</li>
+        </ul>
+        <div class="shop_cart">Ваша корзина:</div>
+    </div>
+    <div class="hidden">
+        <div class="shop_left">
+            <div class="shop_item_loop list-products">
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+@extends('layout')
+
+@section('content')
 <script type="text/javascript" src="{{ asset('new/js/jquery.multiple.select.js') }}"></script>
 <script type="text/javascript" src="{{ asset('new/js/jquery-ui.min.js') }}"></script>
 <div class="content_bg">
@@ -91,34 +150,16 @@
                 var i = 0;
                 response.forEach(function(item){
                     i++;
-                    html += '<div class="item left">';
-                    html += '<div class="image">';
-                    html += '<img src="https://steamcommunity-a.akamaihd.net/economy/image/class/730/'+ item.classId +'/120fx120f" alt="">';
+                    html += '<div class="shop_item shop_item_c1">';
+                    html += '<div class="shop_item_w"><img src="https://steamcommunity-a.akamaihd.net/economy/image/class/730/'+ item.classId +'/120fx120f" /></div>';
+                    html += '<div class="shop_item_n ell">'+ item.name +'</div>';
+                    html += '<div class="shop_item_r">'+ item.price +' <span>руб.</span></div>';
+                    html += '<div class="shop_item_n ell"><a class="buyItem" href="#" data-item="'+ item.id +'">Купить</a></div>';
                     html += '</div>';
-                    html += '<div class="wrapper-store">';
-                    html += '<h2>'+ item.name +'</h2>';
-                    html += '<div class="chars">';
-                    html += '<ul>';
-                    html += '<li><span class="gray">Редкость:</span> <span class="'+ getRarity(item.rarity) +'">'+ item.rarity +'</span></li>';
-                    html += '<li><span class="gray">Качество:</span> '+ item.quality +'</li>';
-                    //html += '<li><span class="gray">Цена в стиме:</span> '+ item.steam_price +' руб.</li>';
-                    html += '</ul>';
-                    html += '</div>';
-                    html += '<div class="price left">'+ item.price +' руб.</div>';
-                    html += '<div class="buy right">';
-                    html += '<a class="buyItem" data-item="'+ item.id +'" href="#">Купить</a>';
-                    html += '</div>';
-                    html += '<div class="clr-b"></div>';
-                    html += '</div>';
-                    html += '</div>';
-                    if(i == 3){
-                        html += '<div class="clr-b"></div>';
-                        i=0;
-                    }
                 })
                 $('.list-products').html(html);
-                $('#countItems').show();
-                $('#paginator').html(response.pages);
+                //$('#countItems').show();
+                //$('#paginator').html(response.pages);
                 
 
                 $('.buyItem').click(function () {
