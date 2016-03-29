6159a97e452590f81df3e7544474bfe23dd3c3a8
Sergo <sergo@mh00.net>
2016-03-29 10:35:48 +0300

    '1'

--- resources/views/pages/history.blade.php
+++ resources/views/pages/history.blade.php
@@ -18,7 +18,7 @@
     <div class="hist_chance chance">
         <ul>
             @foreach($game->chance as $c)
-            <li><b>{{ $c['chance'] }}%</b><img src="{{ $c['avatar'] }}" alt="" /></li>
+            <li><b>{{ $c->chance }}%</b><img src="{{ $c->avatar }}" alt="" /></li>
             @endforeach
         </ul>
     </div>
