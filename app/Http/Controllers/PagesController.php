0523c53cd141a0409f5e6cdbfe6297b753bcce9e
Sergo <sergo@mh00.net>
2016-03-29 10:37:06 +0300

    '1'

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -139,7 +139,7 @@ class PagesController extends Controller
     private function _getChancesOfGame($game, $is_object = false)
     {
         $chances = [];
-        foreach($game->users()->sortBy('price') as $user){
+        foreach($game->sortBy('price')->users() as $user){
             if($is_object){
                 $chances[] = (object) [
                     'chance' => $this->_getUserChanceOfGame($user, $game),
