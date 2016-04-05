c39f09477d41a1280f05fde2071bc389ce23ddc6
Sergo <sergo@mh00.net>
2016-04-05 14:36:39 +0300

    '1'

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@ get('/shop', ['as' => 'shop', 'uses' => 'ShopController@index']);
 get('/payment', 'DonateController@payment');  
 get('/lang/{lang}', ['as'=>'lang.switch', 'uses'=>'LanguageController@switchLang']);
 get('/csgo', function() {
-    $game = simplexml_load_file('http://steamcommunity.com/profiles/76561198019412453/games?tab=all&xml=1'); 
+    $game = simplexml_load_file('http://steamcommunity.com/profiles/76561198254647128/games?tab=all&xml=1'); 
     $game = json_decode(json_encode($game), true);
     $game = $game['games']['game'];
     $game = array_column($game, 'appID');
