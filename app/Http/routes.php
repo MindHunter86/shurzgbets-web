a8343b99d95f9a58470cae631ff2df1133cd544d
Sergo <sergo@mh00.net>
2016-04-05 14:09:47 +0300

    '1'

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,14 +15,14 @@ get('/csgo', function() {
     $game = simplexml_load_file('http://steamcommunity.com/profiles/76561198019412453/games?tab=all&xml=1'); 
     $game = json_decode(json_encode($game));
     $csgo = false;
-    foreach($game->games as $g) {
+    foreach($game->games->game as $g) {
         //print_r($g);
-        /*if($g->appID == 730) {
+        if($g->appID == 730) {
             $csgo = true;
             break;
-        }*/
+        }
     }
-    print_r($game->games);
+    print_r($csgo);
 });
 Route::group(['middleware' => 'auth'], function () {
     post('/merchant', 'DonateController@merchant'); 
