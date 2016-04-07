586529d303f3d37eaf01fb591aab26c9f0efd8fe
Sergo <sergo@mh00.net>
2016-04-07 11:17:28 +0300

    '1'

--- resources/views/admin/game.blade.php
+++ resources/views/admin/game.blade.php
@@ -3,23 +3,23 @@
 @section('content')
 <section class="content">
 <div class="row">
-	<div class="col-md-6">
+	<div class="col-md-12">
       <!-- Horizontal Form -->
       <div class="box box-info">
         <div class="box-header with-border">
-          <h3 class="box-title">Вещи из игры с учетом комиссии</h3>
+          <h3 class="box-title">Вещи из игры с учетом комиссии {{ $game->id }}</h3>
         </div><!-- /.box-header -->
         <!-- form start -->
           <div class="box-body">
             @foreach(json_decode($game->won_items) as $w)
-                <span>
+                 <div class="col-md-1">
                   @if(!isset($w->img))
                     <img style="width: 70px;" src="https://steamcommunity-a.akamaihd.net/economy/image/class/{{ \App\Http\Controllers\GameController::APPID }}/{{ $w->classid }}/200fx200f" alt="" />
                   @else
                     <img src="{{ $w->img }}" alt="" />
                   @endif
                   <p>{{ $w->name }}</p>
-                </span>
+                </div>
             @endforeach
           </div><!-- /.box-body -->
       </div><!-- /.box -->
