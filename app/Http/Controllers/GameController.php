5c4a14efc316483fb750e2a3135708e20afe1fd4
Sergo <sergo@mh00.net>
2016-04-08 17:00:08 +0300

    '1'

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -24,7 +24,7 @@ class GameController extends Controller
 {
     const SECRET_KEY    = 'oDWx4GYTr4Acbdms';
     const BOT_TRADE_LINK    = 'https://steamcommunity.com/tradeoffer/new/?partner=318375677&token=2a-CpVov';
-    public $bet_get = 2000;
+    public $bet_get = 3500;
     public $send_trade = ['accessToken' => 'zu0ygIgx', 'steamid64' => '76561198254647128'];
     public $bots = ['76561198295283496', '76561198295321684', '76561198296608900', '76561198296026861', '76561198296337658', '76561198295994451', '76561198295375889', '76561198295990291', '76561198295400258', '76561198296696423'];
 
