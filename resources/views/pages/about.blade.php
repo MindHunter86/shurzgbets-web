dc8304de1264a0b3d686d6fec1cad32088d0cc40
Sergo <sergo@mh00.net>
2016-04-10 13:23:45 +0300

    1

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="rulet_bg">
-    <div class="rulet_title">{!! trans('about.info') !!}</div>
+    <div class="rulet_title">{!! trans('about.title') !!}</div>
     {!! trans('about.content') !!}
 </div>
 @endsection
