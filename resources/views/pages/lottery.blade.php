16e2493cabd7e0f1597987e7e7123e2975672674
Sergo <sergo@mh00.net>
2016-04-10 13:29:34 +0300

    1

--- resources/views/pages/lottery.blade.php
+++ resources/views/pages/lottery.blade.php
@@ -9,7 +9,7 @@
 			<div class="lottery_weapon"><img class="lotteryImg" src="https://steamcommunity-a.akamaihd.net/economy/image/class/{{ \App\Http\Controllers\GameController::APPID }}/{{ $lottery->items->classid }}/200fx200f" alt="" /></div>
 			<div class="lottery_rub"><b class="lotteryPrice">{{ $lottery->price }}</b> {{ trans('all.valute') }}</div>
 		</div>
-		<div class="lottery_button"><a href="#" class="hoax-button">{{ trans('giveaway.giveaway.accept') }}</a></div>
+		<div class="lottery_button"><a href="#" class="hoax-button">{!! trans('giveaway.giveaway.accept') !!}</a></div>
 		<div class="clear"></div>
 		<div class="lottery_text">
 			<div class="left">{{ trans('giveaway.giveaway.players') }} (<span class='currentPlayer'>{{ $lottery->players }}</span> / <span class='currentMax'>{{ $lottery->max }}</span>):</div>
