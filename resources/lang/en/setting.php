4c2b9e16651be16a11fa8d0ccee5e9bbb13a0a1f
Sergo <sergo@mh00.net>
2016-04-10 13:38:43 +0300

    1

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,10 @@
+<?
+return [
+	'title' => '<b>SETTING</b>',
+	'setting' => [
+		'title' => 'You must enter a reference to the exchange',
+		'why' => 'Where can i get a link?',
+		'help' => 'Be sure to make your inventory public in Steam to receive the prize!',
+		'save' => 'Save settings'
+	]
+];
--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 
 <div class="rulet_bg">
-    <div class="rulet_title"><b>НАСТРОЙКИ</b></div>
+    <div class="rulet_title">{!! trans('setting.title') !!}</div>
     <div class="notrade rulet_bg">
-        <div class="notrade_text">Необходимо ввести ссылку на обмен. <a href="http://steamcommunity.com/id/me/tradeoffers/privacy#trade_offer_access_url">Где взять ссылку?</a></div>
+        <div class="notrade_text">{{ trans('setting.setting.title') }} <a href="http://steamcommunity.com/id/me/tradeoffers/privacy#trade_offer_access_url">{{ trans('setting.setting.why') }}</a></div>
         <input class="notrade_link trade_link" type="text" value="{{ $u->trade_link }}" />
-        <div class="notrade_text2">Обязательно убедитесь что ваш инвентарь публично доступен в Steam для получения приза!</div>
-        <div class="notrade_button"><a href="#" class="save-link2">Запомнить ссылку</a></div>
+        <div class="notrade_text2">{{ trans('setting.setting.help') }}</div>
+        <div class="notrade_button"><a href="#" class="save-link2">{{ trans('setting.setting.save') }}</a></div>
     </div>
 </div>
 @endsection
