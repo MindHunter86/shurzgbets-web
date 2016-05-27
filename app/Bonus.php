4fb17d0035cb9062e9d2b73c635cff6451d0e5aa
frenzy <frenzzy.ekb@gmail.com>
2016-05-28 02:56:09 +0500

    Изменение модели бонусов

--- app/Bonus.php
+++ app/Bonus.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Bonus extends Model
 {
 	protected $table = 'bonus';
-    protected $fillable = ['name', 'market_hash_name', 'classid', 'price', 'rarity'];
+    protected $fillable = ['classid', 'assetid'];
+
+    public function item() {
+        return $this->hasOne('App\Item','classid','classid');
+    }
 }
