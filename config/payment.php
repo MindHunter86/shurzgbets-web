bef2b0a044fca34628bf921eaf1ccaebe8477595
stringnick <string.nick@yandex.ru>
2016-09-14 21:43:21 +0300

    FreeKassa

---
+++ config/payment.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'merchantId' => '12046',//id магазина
+    'secret1'  => 'steammagazine1', //секретное слово №1
+    'secret2'  => 'steammagazine2', //секретное слово №2
+    'url'		 => 'https://shurzgbets.b0q.ru'
+];
--- config/robokassa.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'login'      => 'shurzgbetscom',
-    'password1'  => 'LarzlrMdi9taQ6BCIDJ49MGilW95PUS4', //PBcnB1JHTy8Tp69g4xHI
-    'password2'  => 'SWNQuLVPB7wyQApV3ZmXfnNGBSviobHJ', //V3UKAkw19xr3VPUOme5N
-    'testmode'   => false,
-    'url'		 => 'https://shurzgbets.com'
-];
